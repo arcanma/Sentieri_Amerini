5429255da9a554ffa2221282eb880cfb3715e19e
arcanma <41625131+arcanma@users.noreply.github.com>
2020-04-19 19:19:06 +0200

    Add files via upload

---
+++ map/data/comprare_2.js
@@ -0,0 +1,171 @@
+var json_comprare_2 = {
+	"type":"FeatureCollection",
+	"name":"comprare_2",
+	"crs":{
+		"type":"name",
+		"properties":{
+			"name":"urn:ogc:def:crs:OGC:1.3:CRS84"
+		}
+	},
+	"features":[{
+		"type":"Feature",
+		"properties":{
+			"website":"http:\/\/cantinasandonna.com\/",
+			"phone":"+39 0744 992274",
+			"name":"Cantina Sandonna",
+			"email":"cantinasandonna@gmail.com",
+			"info-it":"https://www.sentieriamerini.net/madein/2017-sandonna",
+			"info-en":"https://www.sentieriamerini.net/madein/2017-sandonna-thk36",
+			"info-de":"https://www.sentieriamerini.net/madein/2017-sandonna-d9ztg"
+		},
+			"geometry":{
+				"type":"Point",
+				"coordinates":[12.354504165350001,42.517542378590001]
+			}
+		},
+		{
+		"type":"Feature",
+		"properties":{
+			"website":"http:\/\/www.cantinalecrete.it",
+			"phone":"+39 328 7011808",
+			"name":"Azienda Vitivinicola Le Crete",
+			"email":"az.agr.lecrete@virgilio.it",
+			"info-it":"https://www.sentieriamerini.net/madein/2017-lecrete",
+			"info-en":"https://www.sentieriamerini.net/madein/2017-lecrete-g6nf8",
+			"info-de":"https://www.sentieriamerini.net/madein/2017-lecrete-dsps3"
+		},
+			"geometry":{
+				"type":"Point",
+				"coordinates":[12.32708930433,42.502557075399999]
+			}
+		},
+		{
+		"type":"Feature",
+		"properties":{
+			"phone":"+39 0744 982619",
+			"name":"Pasta fresca Cerasi Manola",
+			"info-it":"https://www.facebook.com/PastafrescaCerasiManola/",
+			"info-en":"https://www.facebook.com/PastafrescaCerasiManola/",
+			"info-de":"https://www.facebook.com/PastafrescaCerasiManola/" 
+		},
+			"geometry":{
+				"type":"Point",
+				"coordinates":[12.4161816,42.553838]
+			}
+		},
+		{
+		"type":"Feature",
+		"properties":{
+			"website":"http:\/\/www.ilfrantoio.com\/",
+			"phone":"+39 0744 982201",
+			"name":"Frantoio Suatoni",
+			"email":"info@ilfrantoio.com",
+			"info-it":"https://www.sentieriamerini.net/madein/2017--wzm63",
+			"info-en":"https://www.sentieriamerini.net/madein/2017--wzm63-gcxa7",
+			"info-de":"https://www.sentieriamerini.net/madein/2017--wzm63-xx4ke"
+		},
+			"geometry":{
+				"type":"Point",
+				"coordinates":[12.4286243,42.5508139]
+			}
+		},
+		{
+		"type":"Feature",
+		"properties":{
+			"website":"http:\/\/www.cantinezanchi.it\/",
+			"phone":"+39 0744 970011",
+			"name":"Azienda Agricola Zanchi",
+			"email":"info@cantinezanchi.it",
+			"info-it":"https://www.sentieriamerini.net/madein/2017-zanchi",
+			"info-en":"https://www.sentieriamerini.net/madein/2017-zanchi-thk88",
+			"info-de":"https://www.sentieriamerini.net/madein/2017-zanchi-wyrg6"
+		},
+		"geometry":{
+			"type":"Point",
+			"coordinates":[12.40540444314,42.522723822499998]
+			}
+		},
+		{
+		"type":"Feature"
+		,
+		"properties":{
+			"website":"http:\/\/www.oleificiobartolomei.it\/it\/",
+			"phone":"+39 0744 951395",
+			"name":"Vecchio Frantoio Bartolomei",
+			"email":"info@oleificiobartolomei.it",
+			"info-it":"https://www.sentieriamerini.net/madein/2017-",
+			"info-en":"https://www.sentieriamerini.net/madein/2017--c24h9",
+			"info-de":"https://www.sentieriamerini.net/madein/2017--ep5p7"
+		},
+		"geometry":{
+			"type":"Point",
+			"coordinates":[12.2810653,42.6660697]
+			}
+		},
+		{
+		"type":"Feature"
+		,
+		"properties":{
+			"website":"http:\/\/www.italyheart.it\/",
+			"phone":"+39 393 8862901",
+			"name":"Frantoio Italyheart",
+			"email":"info@italyheart.it",
+			"info-it":"https://www.sentieriamerini.net/madein/2017--wzm63-t6nmh",
+			"info-en":"https://www.sentieriamerini.net/madein/2017--wzm63-t6nmh-mn9sn",
+			"info-de":"https://www.sentieriamerini.net/madein/2017--wzm63-t6nmh-egy2c"
+		},
+		"geometry":{
+			"type":"Point",
+			"coordinates":[12.4510021,42.549116]
+			}
+		},
+		{
+		"type":"Feature"
+		,
+		"properties":{
+			"website":"https:\/\/www.birrificioamerino.it\/",
+			"phone":"+39 339 7529420",
+			"name":"Birrificio Amerino",
+			"email":"birrificioamerino@gmail.com",
+			"info-it":"https://www.sentieriamerini.net/madein/2017-birrificioamerino",
+			"info-en":"https://www.sentieriamerini.net/madein/2017-birrificioamerino-mm7sm",
+			"info-de":"https://www.sentieriamerini.net/madein/2017-birrificioamerino-d9k75"
+		},
+		"geometry":{
+			"type":"Point",
+			"coordinates":[12.6467845,42.5599262]
+			}
+		},
+		{
+		"type":"Feature"
+		,
+		"properties":{
+			"phone":"+39 0744 981412",
+			"name":"ConVitto",
+			"info-it":"https://www.sentieriamerini.net/madein/2017-convitto",
+			"info-en":"https://www.sentieriamerini.net/madein/2017-convitto-r86zw",
+			"info-de":"https://www.sentieriamerini.net/madein/2017-convitto-nezdt"
+		},
+		"geometry":{
+			"type":"Point",
+			"coordinates":[12.416240572258834,42.55436359233674]
+			}
+		},
+		{
+		"type":"Feature",
+		"properties":{
+			"website":"http:\/\/gelateriagirotti.com\/",
+			"phone":"+39 0744 981316",
+			"name":"Gelateria Girotti",
+			"email":"info@gelateriagirotti.com",
+			"info-it":"https://www.facebook.com/GelateriaGirotti/",
+			"info-en":"https://www.facebook.com/GelateriaGirotti/",
+			"info-de":"https://www.facebook.com/GelateriaGirotti/"
+		},
+		"geometry":{
+			"type":"Point",
+			"coordinates":[12.416085,42.5546012]
+			}
+		}
+	]
+}
